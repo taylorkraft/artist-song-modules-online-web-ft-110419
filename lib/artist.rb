48adad0c6359afcd3c64bf9b65fe2a4cd9f8e598
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-22 17:08:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,9 +12,9 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  #def self.find_by_name(name)
+  #  @@artists.detect{|a| a.name == name}
+  #end
 
   def self.all
     @@artists
@@ -29,7 +29,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+#  def to_param
+#    name.downcase.gsub(' ', '-')
+#  end
 end
