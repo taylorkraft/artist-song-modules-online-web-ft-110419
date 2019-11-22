6ddbf14fbd197b226ff5a1be1335363e363fc038
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-22 17:25:37 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
-module ClassMethods
-  def self.count
-    self.all.count
+  module ClassMethods
+    def self.count
+      self.all.count
+    end
+
+    def self.reset_all
+        self.all.clear
+    end
   end
 
-  def self.reset_all
-    self.all.clear
+  module InstanceMethods
+    def intitialize
+      self.class.all << self 
+    end
   end
- end
 end
